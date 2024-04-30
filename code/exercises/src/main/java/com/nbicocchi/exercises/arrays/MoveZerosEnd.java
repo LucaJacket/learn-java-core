856bb9a148ad44ac91750089237861d08637589b
Luca Giacchetta <luke.jacket2001@gmail.com>
2024-04-30 17:49:59 +0200

    Test

--- code/exercises/src/main/java/com/nbicocchi/exercises/arrays/MoveZerosEnd.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/arrays/MoveZerosEnd.java
@@ -2,15 +2,15 @@ package com.nbicocchi.exercises.arrays;
 
 public class MoveZerosEnd {
     public static int[] moveZerosEnd(int[] v) {
-        int[] output = new int[v.length];
+        int[] out = new int[v.length];
         int j = 0;
 
         for (int k : v) {
             if (k != 0) {
-                output[j++] = k;
+                out[j++] = k;
             }
         }
 
-        return output;
+        return out;
     }
 }
